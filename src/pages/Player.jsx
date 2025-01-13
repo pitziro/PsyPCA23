b8a965bc0705247bc8399caab8fa2dbccd8d3dc5
pitziro <msarmientor1@gmail.com>
2025-01-13 14:43:52 -0500

    update:teamData

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -4,18 +4,26 @@ import graduationSVG from '../assets/svg/graduation.svg'
 import diplomaSVG from '../assets/svg/diplomas.svg'
 import calendarSVG from '../assets/svg/calendar.svg'
 import locationSVG from '../assets/svg/location.svg'
+import audienceSVG from '../assets/svg/audience.svg'
+
 export default function Player({ player }) {
 	return (
 		<div className={pStyle.p_card}>
-			{/* <p className={pStyle.p_title}>{player.nombre}</p> */}
-			<div className={pStyle.p_imgFrame}>
-				<div
-					className={pStyle.p_image}
-					style={{ backgroundImage: `url(${player.rutaImg})` }}
-					alt="Avatar"
-					role="img"
-					loading="lazy"
-				/>
+			<div>
+				<div className={pStyle.p_imgFrame}>
+					<div
+						className={pStyle.p_image}
+						style={{ backgroundImage: `url(${player.rutaImg})` }}
+						alt="Avatar"
+						role="img"
+						loading="lazy"
+					/>
+				</div>
+				<section className={pStyle.p_cardBtns}>
+					<Link to={player.waUrl} className={pStyle.p_cardBtn}>
+						Agenda tu cita
+					</Link>
+				</section>
 			</div>
 
 			<div className={pStyle.p_cardContent}>
@@ -23,28 +31,38 @@ export default function Player({ player }) {
 					<p className={pStyle.p_cardTitleName}>{player.nombre}</p>
 					<p className={pStyle.p_cardSubTitle}>{player.especialidad}</p>
 				</section>
-				<section className={pStyle.p_cardContentSection}>
-					<img src={graduationSVG} alt="graduation" />
-					<p className={pStyle.p_cardSubTitle}>{player.educacion}</p>
-				</section>
-				<section className={pStyle.p_cardContentSection}>
-					<img src={diplomaSVG} alt="estudios" />
-					<p className={pStyle.p_cardSubTitle}>{player.tecnica}</p>
-				</section>
-				<section className={pStyle.p_cardContentSection}>
-					<img src={calendarSVG} alt="horario" />
-					<p className={pStyle.p_cardSubTitle}>{player.horario}</p>
-				</section>
-				<section className={pStyle.p_cardContentSection}>
-					<img src={locationSVG} alt="location" />
-					<p className={pStyle.p_cardSubTitle}>{player.modalidad}</p>
-				</section>
-
-				<section className={pStyle.p_cardBtns}>
-					<Link to={player.waUrl} className={pStyle.p_cardBtn}>
-						Agenda tu Cita
-					</Link>
-				</section>
+				<div className={pStyle.p_cardDetails}>
+					<section className={pStyle.p_cardContentSection}>
+						<img src={graduationSVG} alt="educacion" />
+						<span className={pStyle.p_cardSubTitle}>
+							{player.educacion}
+						</span>
+					</section>
+					<section className={pStyle.p_cardContentSection}>
+						<img src={diplomaSVG} alt="certificados" />
+						<span className={pStyle.p_cardSubTitle}>
+							{player.tecnica}
+						</span>
+					</section>
+					<section className={pStyle.p_cardContentSection}>
+						<img src={audienceSVG} alt="Especilidad" />
+						<span className={pStyle.p_cardSubTitle}>
+							{player.audiencia}
+						</span>
+					</section>
+					<section className={pStyle.p_cardContentSection}>
+						<img src={calendarSVG} alt="horario" />
+						<span className={pStyle.p_cardSubTitle}>
+							{player.horario}
+						</span>
+					</section>
+					<section className={pStyle.p_cardContentSection}>
+						<img src={locationSVG} alt="ubicacion" />
+						<span className={pStyle.p_cardSubTitle}>
+							{player.modalidad}
+						</span>
+					</section>
+				</div>
 			</div>
 		</div>
 	)
