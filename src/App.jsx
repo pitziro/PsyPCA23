0db7889eb7fa54ae8423ba001e2fc563b50488e4
pitziro <msarmientor1@gmail.com>
2025-07-02 20:17:03 -0500

    robots and SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 import Navbar from './components/top/Navbar'
 import Footer from './components/bottom/Footer'
@@ -45,6 +47,7 @@ function App() {
 				<Footer />
 			</div>
 			<Analytics />
+			<SpeedInsights />
 		</HelmetProvider>
 	)
 }
